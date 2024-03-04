c37c69d2e7eeadf79bd6a6b912820c6801150f64
Gil Barbara <gilbarbara@gmail.com>
2024-03-04 15:57:07 -0300

    Fix template-typescript test

--- template-typescript/src/index.test.ts
+++ template-typescript/src/index.test.ts
@@ -13,14 +13,13 @@ describe('sleep', () => {
     { input: 0.1, timeout: 100 },
     { input: undefined, timeout: 100 },
   ])('should halt the execution for $input', async ({ input, timeout }) => {
-    // eslint-disable-next-line unicorn/consistent-function-scoping
     const fn = async () => {
       await sleep(input);
 
       return 'finished';
     };
 
-    expect(await fn()).toBe('finished');
+    await expect(fn()).resolves.toBe('finished');
     expect(setTimeoutSpy).toHaveBeenLastCalledWith(expect.any(Function), timeout);
   });
 });
